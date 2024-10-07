a65733eed5f2ab2a71dc6da1065dd979856b2087
ashir1231 <contact.ashirch@gmail.com>
2024-10-07 14:11:47 +0500

    Folder structure added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="Generative AI Project",
+    version='0.0.0',
+    author="Ashir Mumtaz",
+    author_email="ashirmumtaz@hotmail.com",
+    packages=find_packages(),
+    install_requires = []
+
+)
